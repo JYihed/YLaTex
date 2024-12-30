0e12ce518d7f668c3c9d245c2c9fe6a41af02fad
@JYhed <2278735190@qq.com>
2024-12-30 10:36:39 +0800

    项目开始

---
+++ src/main/java/com/itcjy/ylatex/YLaTexApplication.java
@@ -0,0 +1,13 @@
+package com.itcjy.ylatex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YLaTexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YLaTexApplication.class, args);
+    }
+
+}
